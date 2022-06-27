49a58cd2c1b1c38d0a2e16ee6ab27806cfcfc4ad
onigirijo <sungik0502@gmail.com>
2022-06-27 17:05:56 +0900

    머신러닝교과서 1/3

---
+++ Machine learning/LogisticRegressionGD.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+class LogisticRegressionGD(object):
+    """경사 하강법을 사용한 로지스틱 회귀 분류기
+
+    매개변수
+    ------------
+    eta : float
+      학습률 (0.0과 1.0 사이)
+    n_iter : int
+      훈련 데이터셋 반복 횟수
+    random_state : int
+      가중치 무작위 초기화를 위한 난수 생성기 시드
+
+    속성
+    -----------
+    w_ : 1d-array
+      학습된 가중치
+    cost_ : list
+      에포크마다 누적된 로지스틱 비용 함수 값
+
+    """
+    def __init__(self, eta=0.05, n_iter=100, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """훈련 데이터 학습
+
+        매개변수
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+          n_samples 개의 샘플과 n_features 개의 특성으로 이루어진 훈련 데이터
+        y : array-like, shape = [n_samples]
+          타깃값
+
+        반환값
+        -------
+        self : object
+
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            
+            # 오차 제곱합 대신 로지스틱 비용을 계산합니다.
+            cost = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output)))
+            self.cost_.append(cost)
+        return self
+    
+    def net_input(self, X):
+        """최종 입력 계산"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, z):
+        """로지스틱 시그모이드 활성화 계산"""
+        # 대신 from scipy.special import expit; expit(z) 을 사용할 수 있습니다.
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250))) #z값을 -250 ~ 250으로 제한.. overflow, underflow 제한
+
+    def predict(self, X):
+        """단위 계단 함수를 사용하여 클래스 레이블을 반환합니다"""
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+        # 다음과 동일합니다.
+        # return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
